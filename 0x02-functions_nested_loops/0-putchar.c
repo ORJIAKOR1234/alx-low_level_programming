388e6a6e61e825ff96147198e61e50277896c1f8
glory <gloriaorjiakor@gmail.com>
2022-09-14 12:55:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include 'main.h'
+
+/**
+ * main _ printer putchar
+ *Return: 0 always
+*/
 int main(void)
-{
-   _ putchar('p');                                                                                                      
-   _putchar('u');                                                                                                       
-  _putchar('t');                                                                                                        
-   _putchar('c');                                                                                                       
-   _putchar('h');                                                                                                       
-   _putchar('a');                                                                                                       
-   _putchar('r');                                                                                                       
-   _putchar('\n');                                                                                                      
-                                                                                                                        
-   return (0);                                                                                                          
+{ 
+   _putchar('p');
+   _putchar('u');
+  _putchar('t');
+   _putchar('c');
+   _putchar('h');
+   _putchar('a');
+   _putchar('r');
+   _putchar('\n');
+
+   return (0);
 }
+
