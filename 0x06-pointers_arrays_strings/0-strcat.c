06e8d504dff4da7986cfed061aeb484502b7af88
glory <gloriaorjiakor@gmail.com>
2022-09-21 17:08:11 -0700

    strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,12 @@
  */
 char*_strcat(char*dest,char*src)
 {
-	int index=0,dest_len=0;
-	while(dest[index++])
+	int index = 0,dest_len = 0;
+	while (dest[index++])
 		dest_len++;
 
-	for (index=0; src[index]; index++)
-		dest[dest len++] =src[index];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
