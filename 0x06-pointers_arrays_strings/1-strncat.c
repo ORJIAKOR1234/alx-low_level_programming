01d13f673d49efb15402f8f69839cd85da5df0d2
glory <gloriaorjiakor@gmail.com>
2022-09-21 17:31:11 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+/**
+ * _strncat - concatenate two strings but add inputted number of bytes
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len =0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] =src[index];
+
+	return (dest);
+}
